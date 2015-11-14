6d0febb5bffa731fb16644d1618c464eb0735517
cmrRose <cmr.rose@gmail.com>
2015-11-13 22:53:11 -0800

    add image of Table 19

--- thesis/summary.js
+++ thesis/summary.js
@@ -1,22 +1,29 @@
 function displayTechDetails(techSummary){
    var detailsDiv = document.getElementById("tech_details");
   
-    detailsDiv.innerHTML = "<h2>"+techSummary.title+"</h2><br><p>details about "+techSummary.uses+" this tech...</p>";
+    detailsDiv.innerHTML = "<h2>"+techSummary.title+"</h2><br><p>details about current uses: "+techSummary.uses+"<br><table><tr><td></td></tr></table> </p>";
     
 }
 
 //json of 
 var printSummary = {"title":"Print Maps", 
-            "uses":"fy", 
+            "uses":"uses of print...", 
              "advantages":"d",
               "suggestions":"hdg",
               "notes":"shy"
              };
 
-    GPSSummary = {"title":"GPS devices", 
+  var  GPSSummary = {"title":"GPS devices", 
              "uses":"fj", 
             "advantages":"styj",
               "suggestions":"fhk",
               "notes":"dfr"
              };
  
+var GISSummary = {"title":"GIS Software", 
+             "uses":"fj", 
+            "advantages":"styj",
+              "suggestions":"fhk",
+              "notes":"Search organizations that use GIS software tend to rely on specific people with the expertise to use it."
+             };
+
